8c9857b3dff21127eab4469138d93f6a6d9090b5
Jason <c04jason@gmail.com>
2023-10-25 00:56:51 -0400

    .

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
+	adminId: "399617261230358530", //- Replace UserId with the Discord ID of the admin of the bot
 	token: process.env.token || "", //- Bot's Token
 	clientId: process.env.clientId || "", //- ID of the bot
 	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
@@ -19,7 +19,7 @@ module.exports = {
 	autoPause: true, //- When set to true, music will automatically be paused if everyone leaves the voice channel
 	autoLeave: false, //- When set to true, the bot will automatically leave when no one is in the voice channel (can be combined with 24/7 to always be in voice channel until everyone leaves; if 24/7 is on disconnectTime will add a disconnect delay after everyone leaves.)
 	debug: false, //- Debug mode
-	cookieSecret: "CodingWithSudhan is epic", //- Cookie Secret
+	cookieSecret: "xentriom is epic", //- Cookie Secret
 	website: "http://localhost:4200", //- without the / at the end
 	// You need a lavalink server for this bot to work!!!!
 	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
@@ -34,7 +34,7 @@ module.exports = {
 			secure: true, //- Can be either true or false. Only use true if ssl is enabled!
 		},
 	],
-	embedColor: "#2f3136", //- Color of the embeds, hex supported
+	embedColor: "#fd7f20", //- Color of the embeds, hex supported
 	presence: {
 		// PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
 		status: "online", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
