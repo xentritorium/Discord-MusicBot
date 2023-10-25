4dccc8dcc2a3067464062a0a2aac5d4ec7d57eed
Jason <c04jason@gmail.com>
2023-10-25 01:01:54 -0400

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,28 +6,16 @@ module.exports = async (client, message) => {
   const refront = `^<@!?${client.user.id}>`;
   const mention = new RegExp(refront + "$");
   const debugIdMention = new RegExp(refront + " debug-id ([^\\s]+)");
-  const invite = `https://discord.com/oauth2/authorize?client_id=${
-    client.config.clientId
-  }&permissions=${client.config.inviteScopes.toString().replace(/,/g, "%20")}`;
-
-  const buttons = new MessageActionRow().addComponents(
-    new MessageButton().setStyle("LINK").setLabel("Invite me").setURL(invite),
-    new MessageButton()
-      .setStyle("LINK")
-      .setLabel("Support server")
-      .setURL(`${client.config.supportServer}`)
-  );
 
   if (message.content.match(mention)) {
     const mentionEmbed = new MessageEmbed()
       .setColor(client.config.embedColor)
       .setDescription(
-        `My prefix on this server is \`/\` (Slash Command).\nTo get started you can type \`/help\` to see all my commands.\nIf you can't see it, Please [re-invite](invite) me with the correct permissions.`
+        `My prefix on this server is \`/\` (Slash Command).\nTo get started you can type \`/help\` to see all my commands.`
       );
 
     message.channel.send({
       embeds: [mentionEmbed],
-      components: [buttons],
     });
   }
 
